6c549416da27cc53c971cbf13b68328b442d2fa3
sudh4800@gmail.com <sudh4800@gmail.com>
2025-03-16 23:59:20 +0530

    minor bugs fixed

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-export const supabaseUrl = 'https://rioxntlgjdjzfnvhyhqi.supabase.co   jkebfkj';
+export const supabaseUrl = 'https://rioxntlgjdjzfnvhyhqi.supabase.co';
 const supabaseKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpb3hudGxnamRqemZudmh5aHFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAzNTI2MDcsImV4cCI6MjA1NTkyODYwN30.ZaKjtCzYZXle9rYtiYlV-NoqAB6up_FETrb8Z0lg7cg';
 const supabase = createClient(supabaseUrl, supabaseKey);
